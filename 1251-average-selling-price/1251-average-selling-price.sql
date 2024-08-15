50a2e39b80d7fc397ce2c070b73974eeda98a9f5
ZekiKurt0 <117575249+ZekiKurt0@users.noreply.github.com>
2024-08-15 21:30:02 +0300

    Time: 516 ms (15.87%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,7 +1,8 @@
-WITH cte AS (
-    SELECT p.product_id,p.price,u.units 
-   FROM Prices as p JOIN UnitsSold AS u ON  p.product_id=u.product_id WHERE u.purchase_date BETWEEN p.start_date and p.end_date
-) 
-SELECT product_id, ROUND((SUM(price*units)*1.0/SUM(units)*1.0),2) AS average_price from cte group by product_id
 
-
+select Prices.product_id, 
+isnull(round(sum(Prices.price * UnitsSold.units * 1.0) / sum(UnitsSold.units),2), 0)
+as average_price
+from Prices left join UnitsSold
+on Prices.product_id = UnitsSold.product_id
+and UnitsSold.purchase_date between start_date and Prices.end_date
+group by Prices.product_id
