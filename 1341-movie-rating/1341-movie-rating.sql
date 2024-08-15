600af6df5e1ea629a8d08682dc1ae85e14ee6520
ZekiKurt0 <117575249+ZekiKurt0@users.noreply.github.com>
2024-08-15 23:07:07 +0300

    Time: 630 ms (33.67%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -3,14 +3,15 @@ WITH highname AS (
     FROM MovieRating AS m 
     JOIN Users AS u ON m.user_id = u.user_id 
     GROUP BY u.name 
-    ORDER BY COUNT(*) DESC,u.name asc
+    ORDER BY COUNT(*) DESC,u.name ASC
 ),
 highmovie AS (
-    SELECT TOP 1 a.title AS moviename 
+    SELECT TOP 1 a.title AS moviename
     FROM MovieRating AS b 
-    JOIN Movies AS a ON a.movie_id = b.movie_id 
+    JOIN Movies AS a ON a.movie_id = b.movie_id
+    where b.created_at between '2020-02-01' and '2020-02-29' 
     GROUP BY a.title 
-    ORDER BY ROUND((SUM(rating) / COUNT(*)), 1) DESC,a.title asc
+    ORDER BY ROUND((SUM(rating)*1.0 / COUNT(*)*1.0),3) DESC,a.title ASC
 )
 SELECT names AS results FROM highname
 UNION ALL
